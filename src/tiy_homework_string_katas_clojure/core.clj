bde874e8865b475730c792938dc9469994ce5001
WinBook <win808mac@Winstons-MacBook-2.local>
2016-08-16 15:22:08 -0400

    with Dougs feedback.. converting to let[]

--- src/tiy_homework_string_katas_clojure/core.clj
+++ src/tiy_homework_string_katas_clojure/core.clj
@@ -7,10 +7,9 @@
 ; where "not " has been added to the front. However, if the string
 ; already begins with "not", return the string unchanged.
 
-(defn notString [word] (str "Example of Not String  --> "
-                          (if (str/starts-with? word "not")
+(defn not-string [word] (if (str/starts-with? word "not")
                           (str word)
-                          (str "not " word))))
+                          (str "not " word)))
 
 
 ;Missing Character -
@@ -18,145 +17,145 @@
 ;where the character at the specified index has been removed.
 ;The specified index will be a valid index of in the original string.
 
-(defn missiingCharacter [word, om] (def front (subs word 0 om))
-                                   (def back (subs word (+ om 1)))
-                                   (str "Eample of Missing Character --> " front back))
+(defn missiing-character [word, om] (let [front (subs word 0 om)
+                                          back (subs word (+ om 1))]
+                                          (str front back)))
 
 ;Front Back -
 ;Given a string, return a new string where the first and last characters have been exchanged.
 
-(defn frontBack [word]  (def one (subs word 0, 1))
-                        (def mid (subs word 1, (- (count word) 1)))
-                        (def final (subs word (- (count word) 1)))
-                          (if (<= (count word) 1)
-                           (str "Example of Front Back --> " word)
-                           (str "Example of Front Back --> " final mid one)))
+(defn front-back [word] (let [one (subs word 0, 1)
+                              mid (subs word 1, (- (count word) 1))
+                              final (subs word (- (count word) 1))]
+                              (if (<= (count word) 1)
+                               (str word)
+                               (str final mid one))))
 
 ;Front Three -
 ;Given a string, we'll say that the front is the first 3
 ;characters of the string. If the string length is less than 3,
 ;the front is whatever is there. Return a new string which is 3 copies of the front.
 
-(defn frontThree [word] (def front (subs word 0,3))
-                          (if (<= (count word) 3)
-                            (str "Example of Front Three-> " word)
-                            (str "Example of Front Three-> " front front front)))
+(defn front-three [word] (let [front (subs word 0, 3)]
+                              (if (<= (count word) 3)
+                               (str word)
+                               (str front front front))))
 
 ;Back Around
 ;Given a string, take the last character and return a new string
 ;with the last character added at the front and back,
 ;so "cat" yields "tcatt". The original string will be length 1 or more.
 
-(defn backAround [word] (def lastChar (subs word (- (count word) 1)))
-                        (str "Example of Back Around --> " lastChar word lastChar))
+(defn back-around [word] (let [lastChar (subs word (- (count word) 1))]
+                          (str lastChar word lastChar)))
 
 ;Front 22
 ;Given a string, take the first 2 characters and return the string
 ;with the 2 characters added at both the front and back, so "kitten" yields"kikittenki".
 ;If the string length is less than 2, use whatever characters are there.
 
-(defn front22 [word] (def frontdeuce (subs word 0,2))
-                     (str "Example of Front 22 --> " frontdeuce word frontdeuce))
+(defn front-22 [word] (let [frontdeuce (subs word 0, 2)]
+                        (str frontdeuce word frontdeuce)))
 
 ;Delete Del
 ;Given a string, if the string "del" appears starting at index 1,
 ;return a string where that "del" has been deleted. Otherwise, return the string unchanged.
 
-(defn delDel [word] (if (= (subs word 1, 4) "del")
-                      (str "Example Delete Del --> "(subs word 0, 1) (subs word 4))
-                      (str "Example Delete Del --> " word)))
+(defn del-del [word] (if (= (subs word 1, 4) "del")
+                          (str (subs word 0, 1) (subs word 4))
+                          (str word)))
 
 ;Start Oz
 ;Given a string, return a string made of the first 2 characters (if present),
 ;however include first char only if it is 'o' and include the second only if it is 'z',
 ;so "ozymandias" yields "oz".
 
-(defn startOz [word] (cond
-                          (and (= (subs word 0,1) "o") (=(subs word 1,2) "z")) (str "Example of startOz --> " (subs word 0,2))
-                          (= (subs word 0,1) "o") (str "Example of startOz --> " (subs word 0,1))
-                          (= (subs word 1,2) "z") (str "Example of startOz --> " (subs word 1,2))
-                          :else (str "Example of startOz -->          ")))
+(defn start-oz [word] (cond
+                        (and (= (subs word 0, 1) "o") (= (subs word 1, 2) "z")) (str "Example of startOz --> " (subs word 0, 2))
+                        (= (subs word 0, 1) "o") (str (subs word 0, 1))
+                        (= (subs word 1, 2) "z") (str (subs word 1, 2))
+                        :else (str " ")))
 
 ;End Up
 ;Given a string, return a new string where the last 3 characters are now in upper case.
 ;If the string has less than 3 characters, uppercase whatever is there.
 
-(defn endUp [word] (def last3 (subs word (- (count word) 3)))
-                   (def front (subs word 0, (- (count word) 3)))
-                        (str "Example of End Up --> " front (str/upper-case last3)))
+(defn end-up [word] (let [last3 (subs word (- (count word) 3))
+                          front (subs word 0, (- (count word) 3))]
+                            (str front (str/upper-case last3))))
 
 
 
 (defn -main []
-  (println (notString "a big deal"))
-  (println (notString "nottingham"))
-  (println (notString "North"))
-  (println (notString "Candy"))
-  (println (notString "x"))
-  (println (notString "not bad"))
+  (println "Example of Not String  --> " (not-string "a big deal"))
+  (println "Example of Not String  --> " (not-string "nottingham"))
+  (println "Example of Not String  --> " (not-string "North"))
+  (println "Example of Not String  --> " (not-string "Candy"))
+  (println "Example of Not String  --> " (not-string "x"))
+  (println "Example of Not String  --> " (not-string "not bad"))
   (println)
 
-  (println (missiingCharacter "rolling", 4))
-  (println (missiingCharacter "cascade", 2))
-  (println (missiingCharacter "slow", 3))
-  (println (missiingCharacter "kitten", 0))
-  (println (missiingCharacter "kitten", 5))
-  (println (missiingCharacter "kitten", 4))
+  (println "Eample of Missing Character --> "(missiing-character "rolling", 4))
+  (println "Eample of Missing Character --> "(missiing-character "cascade", 2))
+  (println "Eample of Missing Character --> "(missiing-character "slow", 3))
+  (println "Eample of Missing Character --> "(missiing-character "kitten", 0))
+  (println "Eample of Missing Character --> "(missiing-character "kitten", 5))
+  (println "Eample of Missing Character --> "(missiing-character "kitten", 4))
   (println)
 
-  (println (frontBack "reverse"))
-  (println (frontBack "hello"))
-  (println (frontBack "code"))
-  ;(println (frontBack "a"))
-  (println (frontBack "ab"))
-  (println (frontBack "hotblooded"))
+  (println "Example of Front Back --> "(front-back "reverse"))
+  (println "Example of Front Back --> " (front-back "hello"))
+  (println "Example of Front Back --> " (front-back "code"))
+  (println "Example of Front Back --> " (front-back "hi"))
+  (println "Example of Front Back --> " (front-back "ab"))
+  (println "Example of Front Back --> " (front-back "hotblooded"))
   (println)
 
-  (println (frontThree "java"))
-  (println (frontThree "chocolate"))
-  (println (frontThree "abc"))
-  (println (frontThree "helium"))
-  (println (frontThree "baloon"))
-  (println (frontThree "heavy"))
+  (println "Example of Front Three-> " (front-three "java"))
+  (println "Example of Front Three-> " (front-three "chocolate"))
+  (println "Example of Front Three-> " (front-three "abc"))
+  (println "Example of Front Three-> " (front-three "helium"))
+  (println "Example of Front Three-> " (front-three "baloon"))
+  (println "Example of Front Three-> " (front-three "heavy"))
   (println)
 
-  (println (backAround "lady"))
-  (println (backAround "cat"))
-  (println (backAround "hello"))
-  (println (backAround "vericose"))
-  (println (backAround "a"))
-  (println (backAround "moody"))
+  (println "Example of Back Around --> "  (back-around "lady"))
+  (println "Example of Back Around --> " (back-around "cat"))
+  (println "Example of Back Around --> " (back-around "hello"))
+  (println "Example of Back Around --> " (back-around "vericose"))
+  (println "Example of Back Around --> " (back-around "a"))
+  (println "Example of Back Around --> " (back-around "moody"))
   (println)
 
-  (println (front22 "follow"))
-  (println (front22 "kitten"))
-  (println (front22 "ha"))
-  (println (front22 "abc"))
-  (println (front22 "shadow"))
-  (println (front22 "million"))
+  (println "Example of Front 22 --> " (front-22 "follow"))
+  (println "Example of Front 22 --> " (front-22 "kitten"))
+  (println "Example of Front 22 --> " (front-22 "ha"))
+  (println "Example of Front 22 --> " (front-22 "abc"))
+  (println "Example of Front 22 --> " (front-22 "shadow"))
+  (println "Example of Front 22 --> " (front-22 "million"))
   (println)
 
-  (println (delDel "adele"))
-  (println (delDel "hdelello"))
-  (println (delDel "adelbc"))
-  (println (delDel "ideal"))
-  (println (delDel "pdelarty"))
-  (println (delDel "spdelecial"))
+  (println "Example of Deldel --> "(del-del "adele"))
+  (println "Example of Deldel --> "(del-del "hdelello"))
+  (println "Example of Deldel --> "(del-del "adelbc"))
+  (println "Example of Deldel --> "(del-del "ideal"))
+  (println "Example of Deldel --> "(del-del "pdelarty"))
+  (println "Example of Deldel --> "(del-del "spdelecial"))
   (println)
 
-  (println (startOz "ozymandias"))
-  (println (startOz "bzoo"))
-  (println (startOz "oxxx"))
-  (println (startOz "tropical"))
-  (println (startOz "wo"))
-  (println (startOz "zolies"))
+  (println "Example of startOz --> " (start-oz "ozymandias"))
+  (println "Example of startOz --> " (start-oz "bzoo"))
+  (println "Example of startOz --> " (start-oz "oxxx"))
+  (println "Example of startOz --> " (start-oz "tropical"))
+  (println "Example of startOz --> " (start-oz "wo"))
+  (println "Example of startOz --> " (start-oz "zolies"))
   (println)
 
-  (println (endUp "hello"))
-  (println (endUp "hi there"))
-  (println (endUp "so tired"))
-  (println (endUp "won't work"))
-  (println (endUp "good enough"))
-  (println (endUp "betten than")))
+  (println "Example of End Up --> "(end-up "hello"))
+  (println "Example of End Up --> "(end-up "hi there"))
+  (println "Example of End Up --> "(end-up "so tired"))
+  (println "Example of End Up --> "(end-up "won't work"))
+  (println "Example of End Up --> "(end-up "good enough"))
+  (println "Example of End Up --> "(end-up "betten than")))
 
 (-main)
